a439c7064ea5334e659b11ce300f76930922735f
zinbad <86434030+zachdev1@users.noreply.github.com>
2024-01-19 20:27:05 -0500

    Time: 50 ms (99.18%), Space: 42.4 MB (100.00%) - LeetHub

---
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.c
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int rangeSumBST(struct TreeNode* root, int low, int high) {
+    int sum = 0;
+    
+    if(root->val >= low && root->val <= high){
+        sum += root->val; 
+    }
+    
+    if(root->left != NULL){
+        sum += rangeSumBST(root->left,low,high); 
+    }
+    
+    if(root->right != NULL){
+        sum += rangeSumBST(root->right,low,high);
+     }
+    return sum; 
+}
