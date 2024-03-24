65b9bbed535553ddb849e175c8fd8ded3e94ab17
zinbad <86434030+zachdev1@users.noreply.github.com>
2024-03-23 23:38:53 -0400

    Time: 53 ms (75.61%), Space: 13.8 MB (9.76%) - LeetHub

---
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.c
@@ -0,0 +1,22 @@
+int countMatches(char*** items, int itemsSize, int* itemsColSize, char* ruleKey, char* ruleValue) {
+    int count = 0;
+    
+    int keyIndex;
+    for(int i = 0; i < itemsSize; i++){
+        if(strcmp(ruleKey, "type") == 0){
+            if(strcmp(ruleValue, items[i][0]) == 0){
+                count++;
+            }
+        } else if (strcmp(ruleKey, "color") == 0){
+            if(strcmp(ruleValue, items[i][1]) == 0){
+                count++;
+            }
+        } else if (strcmp(ruleKey, "name") == 0){
+            if(strcmp(ruleValue, items[i][2]) == 0){
+                count++;
+            }
+        }
+    }
+    
+    return count;
+}
